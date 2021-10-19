8a42ea379a842d2d1c0a607cf30d12a9badf35b4
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-19 23:53:21 +0300

    Delete file

--- src/scripts/components/App/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css';
-
-function App() {
-  return (<h1>Project starter</h1>);
-}
-
-export default App;
-
--- src/scripts/index.jsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('container'));

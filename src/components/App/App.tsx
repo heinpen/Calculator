864585f9b5d033b4e3f856a1a29178de8fcc164d
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-22 16:51:41 +0300

    Add comments and refactor code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.scss';
 
 const operands = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const operators = ['/', '×', '-', '+', '='];
 const extraSymbols = ['C', 'CA', '%'];
-let operatorsInARow = 0;
+
+// let operatorsInARow: number = 0;
+
 function App() {
   const [output, setOutput] = useState<number | string>(0);
+  const operatorsInARow = useRef(0);
 
   function createOperandCell(opd: string) {
     return (
@@ -45,12 +48,16 @@ function App() {
   }
 
   function removeSymbols(string: string) {
+    // If last symbol in string equals space then delete three symbols,
+    // because it is operator(Example of operator: ' + ').
     if (string.slice(-1) === ' ') {
       return string.slice(0, -3);
     }
+    // Set output to zero if user removes last symbol.
     if (string.length === 1) {
       return 0;
     }
+    // Remove one symbol.
     return string.slice(0, -1);
   }
 
@@ -59,74 +66,81 @@ function App() {
     const array = string.split(' ');
 
     // Check if last symbol operator.
-    if (operatorsInARow === 0 && array.length >= 1) {
+    if (operatorsInARow.current === 0 && array.length >= 1) {
       const lastSymbol = array[array.length - 1];
       return Number(lastSymbol) / 100;
     }
     return false;
   }
 
+  function handleOperatorClick(symbol: string) {
+    operatorsInARow.current += 1;
+
+    // Prevent multiple operators in a row.
+    if (operatorsInARow.current >= 3) return;
+    if (operatorsInARow.current >= 2 && symbol !== '-') return;
+    if (operatorsInARow.current === 2 && symbol === '-') {
+      setOutput(`${output}${symbol}`);
+      return;
+    }
+
+    switch (symbol) {
+      case '=':
+        // Block equation if output equals zero.
+        if (output) {
+          // eslint-disable-next-line no-eval
+          setOutput(eval(String(output)));
+          operatorsInARow.current = 0;
+        }
+        break;
+      case '×':
+        setOutput(`${output} * `);
+        break;
+      default:
+        setOutput(`${output} ${symbol} `);
+    }
+  }
+
+  function handleExtraSymbolClick(symbol: string) {
+    switch (symbol) {
+      case 'CA':
+        operatorsInARow.current = 0;
+        setOutput(0);
+        break;
+      case 'C': {
+        const newOutput = removeSymbols(String(output));
+        operatorsInARow.current = 0;
+        setOutput(newOutput);
+        break;
+      }
+      case '%': {
+        const number = getNumber(String(output));
+        if (number) {
+          operatorsInARow.current = 0;
+          // eslint-disable-next-line no-eval
+          setOutput(eval(String(number)));
+        }
+      }
+    }
+  }
+
   function handleBodyClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     const button = e.target as HTMLButtonElement;
     const symbol = button.innerHTML;
+    // Do nothing if it is not a calculator button.
+    if (!button.classList.contains('calculator__button')) return;
 
-    if (button.classList.contains('calculator__button')) {
-      if (button.classList.contains('calculator__operator')) {
-        operatorsInARow += 1;
-
-        if (operatorsInARow >= 3) return;
-        if (operatorsInARow >= 2 && symbol !== '-') return;
-        if (operatorsInARow === 2 && symbol === '-') {
-          setOutput(`${output}${symbol}`);
-          return;
-        }
-
-        if (symbol === '=') {
-          if (output) {
-            // eslint-disable-next-line no-eval
-            setOutput(eval(String(output)));
-            operatorsInARow = 0;
-            return;
-          }
-        } else if (symbol === '×') {
-          setOutput(`${output} * `);
-          return;
-        } else if (output) {
-          setOutput(`${output} ${symbol} `);
-          return;
-        }
-      }
-      if (button.classList.contains('calculator__extra-symbol')) {
-        if (symbol === 'CA') {
-          operatorsInARow = 0;
-          setOutput(0);
-          return;
-        }
-        if (symbol === 'C') {
-          const newOutput = removeSymbols(String(output));
-          operatorsInARow = 0;
-          setOutput(newOutput);
-          return;
-        }
-
-        if (symbol === '%') {
-          const number = getNumber(String(output));
-          if (number) {
-            operatorsInARow = 0;
-            // eslint-disable-next-line no-eval
-            setOutput(eval(String(number)));
-            return;
-          }
-          return;
-        }
-      }
+    if (button.classList.contains('calculator__operator')) {
+      handleOperatorClick(symbol);
+    } else if (button.classList.contains('calculator__extra-symbol')) {
+      handleExtraSymbolClick(symbol);
+    } else {
       if (output) {
         setOutput(output + symbol);
-        operatorsInARow = 0;
       } else {
         setOutput(symbol);
-        operatorsInARow = 0;
       }
+      operatorsInARow.current = 0;
     }
   }
   return (
